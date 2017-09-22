cebc0f9553969029a8d0d561d3db3bd3445153e5
frank <xiefs.xjtu@foxmail.com>
2017-09-22 13:20:22 +0800

    docker files

--- sit-base-service-gateway/src/main/java/cn/com/sit/servicegateway/SitBaseServiceGatewayApplication.java
+++ sit-base-service-gateway/src/main/java/cn/com/sit/servicegateway/SitBaseServiceGatewayApplication.java
@@ -1,8 +1,10 @@
 package cn.com.sit.servicegateway;
 
+import filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,4 +13,9 @@ public class SitBaseServiceGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SitBaseServiceGatewayApplication.class, args);
 	}
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 }
---
+++ sit-base-service-gateway/src/main/java/filter/AccessFilter.java
@@ -0,0 +1,41 @@
+package filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletRequest;
+@Slf4j
+public class AccessFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s request to %s , \n  ", request.getMethod(), request.getRequestURI())+"Authorization : "+request.getHeader("Authorization"));
+        String path=request.getRequestURI();
+        String Authorization = request.getHeader("Authorization");
+        if(!path.equals("/token-service/session") && !path.equals("/feigntest/getsession") && (Authorization == null || Authorization.equals(""))) {
+            log.warn("Authorization is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        log.info("access token ok");
+        return null;
+    }
+}
